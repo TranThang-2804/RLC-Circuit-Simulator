3945047c8155431b322496f882237eeee65fffa6
phananh1310 <phananh13102001@gmail.com>
2021-05-11 18:27:13 +0700

    Update DCsource.java

--- Demo_GUI/src/Components/Source/DCsource.java
+++ Demo_GUI/src/Components/Source/DCsource.java
@@ -1,5 +1,17 @@
 package Components.Source;
 
-public class DCsource {
+import java.awt.Image;
+
+public class DCsource extends Source {
+
+	DCsource(double spec, String name) {
+		super(spec, name);
+	}
+
+	@Override
+	public Image getComponentImage(boolean connectType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
